6760fa253d8a970f100d7218021b97ac5ad9473f
veok <p.lelental@gmail.com>
2018-01-21 12:09:09 +0100

    rest init #2

--- src/main/java/pl/npp/nopodajpodajserver/model/user/User.java
+++ src/main/java/pl/npp/nopodajpodajserver/model/user/User.java
@@ -1,15 +1,10 @@
 package pl.npp.nopodajpodajserver.model.user;
 
 import lombok.Data;
-
 import pl.npp.nopodajpodajserver.model.reservation.Reservation;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-
-import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.enabled;
 
 /**
  * @author Paweł Lelental
@@ -32,40 +27,4 @@ public class User {
     private List<Reservation> reservations;
     private boolean enabled;
 
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return new ArrayList<GrantedAuthority>();
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        // we never lock accounts
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        // credentials never expire
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return enabled;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return email;
-//    }
 }
